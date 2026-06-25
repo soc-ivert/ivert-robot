7a6c5129071d0a9e5183a5586032c3f0a0e0fde6
codevinni <code.vinni@gmail.com>
2026-06-25 15:50:50 -0300

    feat: voice input
    Add voice capturing in web interface to get user's input to the agent

--- src/interface/static/scripts/main.js
+++ src/interface/static/scripts/main.js
@@ -2,6 +2,8 @@
 
 import { connect } from './websocket.js';
 import { initChat } from './chat.js';
+import { startListening } from './speech.js';
 
 connect();
 initChat();
+startListening();
--- src/interface/static/scripts/speech.js
+++ src/interface/static/scripts/speech.js
@@ -2,15 +2,48 @@
 
 import { send } from './websocket.js';
 
+let isSpeaking = false;
+
+const recognition = new webkitSpeechRecognition();
+recognition.lang = 'pt-BR';
+recognition.continuous = true;
+recognition.interimResults = false;
+recognition.onresult = (event) => {
+
+    const text = event.results[event.results.length - 1][0].transcript;
+
+    send({ 
+        type: 'ask', 
+        data: { text } 
+    });
+};
+
+recognition.onend = () => {
+    if(!isSpeaking) 
+        recognition.start();
+};
+
+function startListening() {
+    recognition.start();
+}
+
 function speak(text) {
+
+    if(text === "error")
+        text = "Estou indisponível no momento, tente falar comigo mais tarde";
+
+    isSpeaking = true;
+    recognition.stop();
 
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = 'pt-BR';
     utterance.onend = () => {
         send({ type: 'speech_end' });
+        isSpeaking = false;
+        recognition.start();
     };
 
     speechSynthesis.speak(utterance);
 }
 
-export { speak };
+export { speak, startListening };
